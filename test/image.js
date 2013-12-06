604d7fa37ce884c8d1ab8cccce8b07197e083b05
Mike Morris <michael.patrick.morris@gmail.com>
2013-12-06 16:33:06 -0500

    write images to /tmp

--- test/image.js
+++ test/image.js
@@ -2,17 +2,12 @@ var fs = require('fs');
 var util = require('util');
 var path = require('path');
 var gm = require('gm');
-var exec = require('child_process').exec;
-var existsSync = require('fs').existsSync || require('path').existsSync
+var mkdirp = require('mkdirp');
+var crypto = require('crypto');
 
-var graphics_magick_available = true;
-var overwrite = false;
-
-exec('gm compare -help', function(error, stdout, stderr) {
-    if (error !== null) {
-      graphics_magick_available = false;
-    }
-});
+function md5(str) {
+    return crypto.createHash('md5').update(str).digest('hex');
+}
 
 module.exports = function imageEqualsFile(buffer, fixture, callback) {
     var fixturesize = fs.statSync(fixture).size;
@@ -21,23 +16,20 @@ module.exports = function imageEqualsFile(buffer, fixture, callback) {
         return callback(new Error('Image size is too different from fixture: ' + buffer.length + ' vs. ' + fixturesize));
     }
 
-    if (!graphics_magick_available) {
-        throw new Error("graphicsmagick 'compare' tool is not available, please install before running tests");
-    }
-    
-    var type = path.extname(fixture);
-    var actual = path.join(path.dirname(fixture), path.basename(fixture, type) + '.result' + type);
-    fs.writeFile(actual, buffer, function(err) {
-        if (err) return callback(err);
-        var tolerance = 0.008;
-        gm.compare(fixture, actual, tolerance, function(err, isEqual, equality, raw) {
-            if (err) return callback(err);
-            // Clean up old failures.
-            if (existsSync(actual)) fs.unlinkSync(actual);
-            if (!isEqual) {
-                return callback(new Error('Image is too different from fixture: ' + equality + ' > ' + tolerance));
-            }
-            callback();
-        });
+    var dir = '/tmp/tilelive-vector-compare';
+    var actual = path.join(dir, md5(buffer));
+    mkdirp(dir, function(err) {
+      if (err) return callback(err);
+      fs.writeFile(actual, buffer, function(err) {
+          if (err) return callback(err);
+          var tolerance = 0.008;
+          gm.compare(fixture, actual, tolerance, function(err, isEqual, equality, raw) {
+              if (err) return callback(err);
+              if (!isEqual) {
+                  return callback(new Error('Image is too different from fixture: ' + equality + ' > ' + tolerance));
+              }
+              callback();
+          });
+      });
     });
 };
