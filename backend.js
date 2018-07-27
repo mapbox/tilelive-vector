2b07d593d0d1f89060ab1b07c833ba0e52388fe5
mapsam <matthews.sam@gmail.com>
2018-07-27 11:34:02 -0500

    requote

--- backend.js
+++ backend.js
@@ -86,9 +86,9 @@ Backend.prototype.getTile = function(z, x, y, callback) {
         headers['Last-Modified'] = new Date(headers['Last-Modified'] || 0).toUTCString();
 
         // Set an ETag if not present.
-        headers['ETag'] = headers['ETag'] || crypto.createHash('md5')
+        headers['ETag'] = headers['ETag'] || JSON.stringify(crypto.createHash('md5')
             .update((z+','+x+','+y) + (data && data.toString('binary')||''), 'utf8')
-            .digest('hex');
+            .digest('hex'));
 
         // Set content type.
         headers['Content-Type'] = 'application/x-protobuf';
