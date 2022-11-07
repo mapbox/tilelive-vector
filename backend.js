579f7115567cbe875493fe0edef58a78dac66aff
WMDE-Fisch <christoph.jauera@wikimedia.de>
2022-11-07 20:11:24 +0100

    Apply: Pass params to backend, tile|grid|info=>data
    From kartotherian:tilelive-vector

--- backend.js
+++ backend.js
@@ -67,7 +67,7 @@ Backend.prototype.getAsync = function(opts) {
 
 Backend.prototype.getInfo = function(callback) {
     this.getAsync({type: 'info'}).then(res => {
-        callback(undefined, res.info);
+        callback(undefined, res.data);
     }, err => {
         callback(err);
     });
@@ -83,9 +83,10 @@ Backend.prototype.getTile = function(z, x, y, callback) {
         scale: callback.scale,
         raw_buffer: callback.raw_buffer,
         upgrade: callback.upgrade,
-        setSrcData: callback.setSrcData
+        setSrcData: callback.setSrcData,
+        treatAsVector: callback.treatAsVector
     }).then(res => {
-        callback(undefined, res.tile, res.headers);
+        callback(undefined, res.data, res.headers);
     }, err => {
         callback(err);
     });
@@ -157,14 +158,14 @@ function _getTileAsync(opts) {
         }
 
         // Pass-thru of an upstream mapnik vector tile (not pbf) source.
-        if (data instanceof mapnik.VectorTile) return accept({tile: data, headers: headers});
+        if (data instanceof mapnik.VectorTile) return accept({data: data, headers: headers});
 
         var vtile = new mapnik.VectorTile(opts.z, opts.x, opts.y);
         vtile._srcbytes = size;
         if (opts.setSrcData) vtile._srcdata = data;
 
         // null/zero length data is a solid tile be painted.
-        if (!data || !data.length) return accept({tile: vtile, headers: headers});
+        if (!data || !data.length) return accept({data: vtile, headers: headers});
 
         try {
             if (type === 'pbf') {
@@ -172,12 +173,12 @@ function _getTileAsync(opts) {
                 // and is empty so skips a clear call internally in mapnik.
                 vtile.addData(data,{upgrade:upgrade},function(err) {
                     if (err) return reject(err);
-                    return accept({tile: vtile, headers: headers});
+                    return accept({data: vtile, headers: headers});
                 });
             } else {
                 vtile.addImageBuffer(data, backend._layer, function(err) {
                     if (err) return reject(err);
-                    return accept({tile: vtile, headers: headers});
+                    return accept({data: vtile, headers: headers});
                 });
             }
         } catch (err) {
@@ -198,7 +199,7 @@ function _getTileAsync(opts) {
         if (err.message !== 'Tile does not exist') throw err;
         return {};
     }).then(result => {
-        var body = result.tile;
+        var body = result.data;
         var head = result.headers;
 
         if (body instanceof mapnik.VectorTile) {
@@ -208,10 +209,14 @@ function _getTileAsync(opts) {
         }
 
         var compression = false;
-        if (body && body[0] == 0x78 && body[1] == 0x9C) {
-            compression = 'inflate';
-        } else if (body && body[0] == 0x1F && body[1] == 0x8B) {
-            compression = 'gunzip';
+        if (body) {
+            if (body[0] == 0x78 && body[1] == 0x9C) {
+                compression = 'inflate';
+            } else if (body[0] == 0x1F && body[1] == 0x8B) {
+                compression = 'gunzip';
+            } else if (opts.treatAsVector) {
+                compression = true;
+            }
         }
 
         if (!body || !body.length) {
